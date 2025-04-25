4532df4394074733c25188484f7c3ed662d32157
eAreebk <156355720+eAreebk@users.noreply.github.com>
2025-04-25 18:11:18 +0500

    Update ConnectDB.js

--- Utils/ConnectDB.js
+++ Utils/ConnectDB.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 //Try to connect MongoDB, if success show connected or will through error
 export const connectDB = async () => {
     try {
-        const con = await mongoose.connect('mongodb+srv://eareebk79:c0.ak.97@cluster0.l541u.mongodb.net/');
+        const con = await mongoose.connect('');
         //const con = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${con.connection.host}`),
         {
